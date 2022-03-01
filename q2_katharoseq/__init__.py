818d90492824c9e6c6a843e773ea96f9f6976d79
Daniela Perry <danielaperry2015@gmail.com>
2022-03-01 13:23:00 -0800

    import biomass_plot into init

--- q2_katharoseq/__init__.py
+++ q2_katharoseq/__init__.py
@@ -6,7 +6,7 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 from . import _version
-from ._methods import read_count_threshold, estimating_biomass
+from ._methods import read_count_threshold, estimating_biomass, biomass_plot
 
 __version__ = _version.get_versions()['version']
 __all__ = ['read_count_threshold', 'estimating_biomass']
