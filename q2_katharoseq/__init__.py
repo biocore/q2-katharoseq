6f02570ee8c0d6fb507ee99b7a039a83d059276c
Daniela Perry <danielaperry2015@gmail.com>
2022-03-01 14:37:19 -0800

    fix typo

--- q2_katharoseq/__init__.py
+++ q2_katharoseq/__init__.py
@@ -6,7 +6,8 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 from . import _version
-from ._methods import read_count_threshold, estimating_biomass, biomass_plot
+from ._methods import read_count_threshold, estimating_biomass
+from ._methods import biomass_plot
 
 __version__ = _version.get_versions()['version']
 __all__ = ['read_count_threshold', 'estimating_biomass', 'biomass_plot']
